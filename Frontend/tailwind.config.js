c24e910f4b10d8d630255179777ef7cf9883831e
Andrei Bilbo <vladutbilboreanu@gmail.com>
2023-05-13 15:24:09 +0300

    fix: commit for stage changes

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/design/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -11,6 +13,17 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      spacing: {
+        xxs: "5px",
+        xs: "10px",
+        sm: "16px",
+        md: "20px",
+        lg: "24px",
+        xl: "30px",
+        "2xl": "40px",
+        "3xl": "60px",
+        "4xl": "70px",
       },
     },
   },
